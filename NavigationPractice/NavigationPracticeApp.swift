170965546735baacda7ef7b0919259ee4a95383a
Nicole Bond <nicbond22@gmail.com>
2025-04-20 17:19:52 -0400

    Initial Commit

---
+++ NavigationPractice/NavigationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracticeApp.swift
+//  NavigationPractice
+//
+//  Created by Scholar on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
